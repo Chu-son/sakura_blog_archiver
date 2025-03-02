a5dca5eb2828e98e7ca6db7551c566a01c724855
Chu-son <chuson.dev@gmail.com>
2025-03-03 01:19:58 +0900

    feat: 変数名やメソッド名などを修正

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ class MODE(enum.Enum):
 
 
 class LoggerSetup:
-    def __init__(self, save_dir):
-        self.logger = self.setup_logger(save_dir)
+    def __init__(self, log_dir):
+        self.logger = self.setup_logger(log_dir)
 
-    def setup_logger(self, save_dir):
+    def setup_logger(self, log_dir):
         logger = getLogger(__name__)
         logger.setLevel(DEBUG)
         formatter = Formatter(
@@ -28,7 +28,7 @@ class LoggerSetup:
         stream_handler.setLevel(DEBUG)
         stream_handler.setFormatter(formatter)
         logger.addHandler(stream_handler)
-        file_handler = FileHandler(f"{save_dir}/log.log")
+        file_handler = FileHandler(f"{log_dir}/log.log")
         file_handler.setLevel(DEBUG)
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
@@ -45,26 +45,26 @@ class DirectoryManager:
 
 class URLProcessor:
     @staticmethod
-    def delete_unnecessary_query(url: str) -> str:
+    def remove_query(url: str) -> str:
         return re.sub(r"\?ima=\d+", "", url)
 
     @staticmethod
-    def delete_all_query(url: str) -> str:
+    def remove_all_query(url: str) -> str:
         return re.sub(r"\?.*", "", url)
 
     @staticmethod
-    def extract_relative_path(url: str) -> str:
+    def get_relative_path(url: str) -> str:
         path = url.replace("https://", "").replace("http://", "")
         if path[0] == "/":
             path = path[1:]
         return path
 
     @staticmethod
-    def is_home(url: str) -> bool:
-        return "/" == URLProcessor.delete_all_query(url)[-1]
+    def is_home_page(url: str) -> bool:
+        return "/" == URLProcessor.remove_all_query(url)[-1]
 
     @staticmethod
-    def is_outer_link(base_url: str, url: str) -> bool:
+    def is_external_link(base_url: str, url: str) -> bool:
         if url.startswith("http"):
             return not url.startswith(base_url)
         return False
@@ -74,19 +74,19 @@ class URLProcessor:
         return "s/s46/diary/detail" in url
 
     @staticmethod
-    def is_blog_list(url: str) -> bool:
+    def is_blog_list_page(url: str) -> bool:
         return "s/s46/diary/blog/list" in url
 
     @staticmethod
-    def is_profile(url: str) -> bool:
+    def is_profile_page(url: str) -> bool:
         return "s/s46/artist" in url
 
     @staticmethod
-    def is_api(url: str) -> bool:
+    def is_api_endpoint(url: str) -> bool:
         return not url.endswith("/") and len(url.split("/")[-1].split(".")) == 1
 
     @staticmethod
-    def get_url_from_style(style: str) -> str:
+    def extract_url_from_style(style: str) -> str:
         style = style.replace("\n", "")
         match = re.search(r'url\((.*?)\)', style)
         if match:
@@ -96,15 +96,15 @@ class URLProcessor:
 
 class FileManager:
     @staticmethod
-    def save_file(download_url: str, file_local_path: str, logger):
-        if os.path.exists(file_local_path):
-            logger.warning(f"Skip: '{file_local_path}' already exists.")
+    def save_file(download_url: str, local_path: str, logger):
+        if os.path.exists(local_path):
+            logger.warning(f"Skip: '{local_path}' already exists.")
         else:
-            DirectoryManager.prepare_directory(file_local_path)
+            DirectoryManager.prepare_directory(local_path)
             try:
-                response = SakuraBlogArchiver.get_response(download_url)
-                logger.info(f"Contents save to '{file_local_path}'.")
-                with open(file_local_path, "wb") as f:
+                response = SakuraBlogArchiver.fetch_response(download_url)
+                logger.info(f"Contents saved to '{local_path}'.")
+                with open(local_path, "wb") as f:
                     f.write(response.content)
             except requests.exceptions.ConnectionError as e:
                 logger.error(e)
@@ -113,16 +113,16 @@ class FileManager:
 
 class SakuraBlogArchiver:
     def __init__(self):
-        self.MODE_ = MODE.BLOG
-        self.BLOG_LIST_MAX_PAGE = 8
-        self.BLOG_LIST_CURRENT_PAGE = 1
-        self.EXEC_DATE_STR = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        self.SAVE_DIR = os.path.join("./output", self.EXEC_DATE_STR) + "/"
-        DirectoryManager.prepare_directory(self.SAVE_DIR)
-        self.logger = LoggerSetup(self.SAVE_DIR).logger
+        self.mode = MODE.BLOG
+        self.max_blog_list_page = 8
+        self.current_blog_list_page = 1
+        self.execution_date_str = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        self.save_dir = os.path.join("./output", self.execution_date_str) + "/"
+        DirectoryManager.prepare_directory(self.save_dir)
+        self.logger = LoggerSetup(self.save_dir).logger
 
     @staticmethod
-    def get_response(url: str) -> requests.Response:
+    def fetch_response(url: str) -> requests.Response:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
         }
@@ -130,15 +130,14 @@ class SakuraBlogArchiver:
         response = requests.get(url, headers=headers)
         return response
 
-    def get_local_path_dir(self, url: str) -> str:
-        path = os.path.join(
-            self.SAVE_DIR, URLProcessor.extract_relative_path(url))
+    def get_local_directory(self, url: str) -> str:
+        path = os.path.join(self.save_dir, URLProcessor.get_relative_path(url))
         if len(path.split("/")[-1].split(".")) > 1:
             path = os.path.dirname(path)
         return path
 
-    def create_relative_path(self, from_: str, to_: str) -> str:
-        return os.path.relpath(to_, from_)
+    def create_relative_path(self, from_path: str, to_path: str) -> str:
+        return os.path.relpath(to_path, from_path)
 
     def get_next_page_url(self, soup: BeautifulSoup) -> str:
         next_link = soup.find('a', string="次へ")
@@ -148,51 +147,51 @@ class SakuraBlogArchiver:
                 return next_url
         return None
 
-    def get_next_page_url_blog_list(self, soup: BeautifulSoup) -> str:
-        self.BLOG_LIST_CURRENT_PAGE += 1
-        next_link = soup.find('a', string=f"{self.BLOG_LIST_CURRENT_PAGE}")
+    def get_next_blog_list_page_url(self, soup: BeautifulSoup) -> str:
+        self.current_blog_list_page += 1
+        next_link = soup.find('a', string=f"{self.current_blog_list_page}")
         if next_link:
             next_url = next_link.get('href')
             if next_url:
                 return next_url
         return None
 
-    def query_has(self, url: str, query: str) -> bool:
+    def has_query(self, url: str, query: str) -> bool:
         return query in url
 
-    def replace_tag_url(self, tag, target_html, file_local_path):
+    def update_tag_url(self, tag, target_html, local_path):
         p = None
-        if URLProcessor.is_diary_page(URLProcessor.delete_all_query(file_local_path)) or URLProcessor.is_profile(URLProcessor.delete_all_query(file_local_path)) or URLProcessor.is_blog_list(URLProcessor.delete_all_query(file_local_path)):
-            p = self.create_relative_path(os.path.dirname(target_html), URLProcessor.delete_unnecessary_query(
-                file_local_path)).replace("index.html", "local_index.html")
+        if URLProcessor.is_diary_page(URLProcessor.remove_all_query(local_path)) or URLProcessor.is_profile_page(URLProcessor.remove_all_query(local_path)) or URLProcessor.is_blog_list_page(URLProcessor.remove_all_query(local_path)):
+            p = self.create_relative_path(os.path.dirname(target_html), URLProcessor.remove_query(
+                local_path)).replace("index.html", "local_index.html")
             tag['href'] = p
         elif tag.name == 'a' or tag.name == 'link':
             p = self.create_relative_path(os.path.dirname(
-                target_html), URLProcessor.delete_unnecessary_query(file_local_path))
+                target_html), URLProcessor.remove_query(local_path))
             tag['href'] = p
         elif tag.name == 'span':
             style = tag['style']
-            extracted_url = URLProcessor.get_url_from_style(style)
+            extracted_url = URLProcessor.extract_url_from_style(style)
             p = style.replace(extracted_url, self.create_relative_path(
-                os.path.dirname(target_html), URLProcessor.delete_unnecessary_query(file_local_path)))
+                os.path.dirname(target_html), URLProcessor.remove_query(local_path)))
             tag['style'] = p
         elif tag.name == 'object':
             p = self.create_relative_path(os.path.dirname(
-                target_html), URLProcessor.delete_unnecessary_query(file_local_path))
+                target_html), URLProcessor.remove_query(local_path))
             tag['data'] = p
         else:
             p = self.create_relative_path(os.path.dirname(
-                target_html), URLProcessor.delete_unnecessary_query(file_local_path))
+                target_html), URLProcessor.remove_query(local_path))
             tag['src'] = p
         if p:
-            self.logger.info(f"Replace URL to '{p}' in {tag.name} tag.")
+            self.logger.info(f"Updated URL to '{p}' in {tag.name} tag.")
         else:
-            self.logger.warning(f"Replace URL failed in {tag.name} tag.")
+            self.logger.warning(f"Failed to update URL in {tag.name} tag.")
 
     def save_diary_page(self, url: str) -> str:
         base_url = re.search(r"https?://[^/]+", url).group(0)
         self.logger.info(f"Target URL: {url}")
-        response = self.get_response(url)
+        response = self.fetch_response(url)
         self.logger.info(f"status_code: {response.status_code}")
 
         target_html = self.get_target_html_path(url)
@@ -210,19 +209,19 @@ class SakuraBlogArchiver:
         return next_url
 
     def get_target_html_path(self, url: str) -> str:
-        if self.MODE_ == MODE.BLOG_LIST:
-            return os.path.join(self.get_local_path_dir(URLProcessor.delete_all_query(url)), str(self.BLOG_LIST_CURRENT_PAGE), "index.html")
+        if self.mode == MODE.BLOG_LIST:
+            return os.path.join(self.get_local_directory(URLProcessor.remove_all_query(url)), str(self.current_blog_list_page), "index.html")
         else:
-            return os.path.join(self.get_local_path_dir(URLProcessor.delete_all_query(url)), "index.html")
+            return os.path.join(self.get_local_directory(URLProcessor.remove_all_query(url)), "index.html")
 
     def save_response_content(self, response: requests.Response, target_html: str):
         with open(target_html, "w") as f:
             f.write(response.text)
-        self.logger.info(f"Save to '{target_html}'.")
+        self.logger.info(f"Saved to '{target_html}'.")
 
     def get_next_url(self, soup: BeautifulSoup, base_url: str) -> str:
-        next_url = self.get_next_page_url_blog_list(
-            soup) if self.MODE_ == MODE.BLOG_LIST else self.get_next_page_url(soup)
+        next_url = self.get_next_blog_list_page_url(
+            soup) if self.mode == MODE.BLOG_LIST else self.get_next_page_url(soup)
         if next_url:
             next_url = base_url + next_url
             self.logger.info(f"Next URL: {next_url}")
@@ -236,22 +235,22 @@ class SakuraBlogArchiver:
         for tag in tags:
             self.logger.info("-----------")
             download_url = self.get_download_url(tag)
-            if download_url is None or URLProcessor.is_outer_link(base_url, download_url):
-                self.logger.warning(f"Outer link: {download_url}")
+            if download_url is None or URLProcessor.is_external_link(base_url, download_url):
+                self.logger.warning(f"External link: {download_url}")
                 continue
 
             if not download_url.startswith("http"):
                 download_url = base_url + download_url
-            self.logger.info(f"Download from {download_url}")
+            self.logger.info(f"Downloading from {download_url}")
 
-            file_local_path = self.get_file_local_path(tag, download_url)
-            FileManager.save_file(download_url, file_local_path, self.logger)
-            self.replace_tag_url(tag, target_html, file_local_path)
+            local_path = self.get_local_file_path(tag, download_url)
+            FileManager.save_file(download_url, local_path, self.logger)
+            self.update_tag_url(tag, target_html, local_path)
 
     def get_download_url(self, tag) -> str:
         if tag.name == 'span' and 'style' in tag.attrs:
             style = tag['style']
-            return URLProcessor.get_url_from_style(style)
+            return URLProcessor.extract_url_from_style(style)
         elif tag.name == 'a' or tag.name == 'link':
             return tag.get('href')
         elif tag.name in ['img', 'script', 'video', 'audio', 'source', 'embed']:
@@ -262,22 +261,22 @@ class SakuraBlogArchiver:
             self.logger.warning(f"Skip: {tag.name}")
             return None
 
-    def get_file_local_path(self, tag, download_url: str) -> str:
-        if tag.name == 'a' and URLProcessor.is_blog_list(tag.get('href')):
-            if self.query_has(tag.get('href'), "page="):
+    def get_local_file_path(self, tag, download_url: str) -> str:
+        if tag.name == 'a' and URLProcessor.is_blog_list_page(tag.get('href')):
+            if self.has_query(tag.get('href'), "page="):
                 page_num = tag.get_text()
-                return os.path.join(self.get_local_path_dir(URLProcessor.delete_all_query(download_url)), page_num, "index.html")
+                return os.path.join(self.get_local_directory(URLProcessor.remove_all_query(download_url)), page_num, "index.html")
             else:
-                return os.path.join(self.get_local_path_dir(URLProcessor.delete_all_query(download_url)), "index.html")
-        elif URLProcessor.is_api(URLProcessor.delete_all_query(download_url)) or URLProcessor.is_home(URLProcessor.delete_all_query(download_url)):
-            return os.path.join(self.get_local_path_dir(URLProcessor.delete_all_query(download_url)), "index.html")
+                return os.path.join(self.get_local_directory(URLProcessor.remove_all_query(download_url)), "index.html")
+        elif URLProcessor.is_api_endpoint(URLProcessor.remove_all_query(download_url)) or URLProcessor.is_home_page(URLProcessor.remove_all_query(download_url)):
+            return os.path.join(self.get_local_directory(URLProcessor.remove_all_query(download_url)), "index.html")
         else:
-            return os.path.join(self.get_local_path_dir(URLProcessor.delete_unnecessary_query(download_url)), os.path.basename(URLProcessor.delete_all_query(download_url)))
+            return os.path.join(self.get_local_directory(URLProcessor.remove_query(download_url)), os.path.basename(URLProcessor.remove_all_query(download_url)))
 
     def save_local_html(self, soup: BeautifulSoup, local_html: str):
         with open(local_html, "w") as f:
             f.write(soup.prettify())
-        self.logger.info(f"Save local HTML to '{local_html}'.")
+        self.logger.info(f"Saved local HTML to '{local_html}'.")
 
     def loop(self, url: str):
         start_time = time.time()
@@ -293,9 +292,9 @@ class SakuraBlogArchiver:
         self.logger.info(f"Count: {count}")
 
     def main(self):
-        self.logger.info(f"Save to '{self.SAVE_DIR}' directory.")
-        if not os.path.exists(self.SAVE_DIR):
-            os.mkdir(self.SAVE_DIR)
+        self.logger.info(f"Saving to '{self.save_dir}' directory.")
+        if not os.path.exists(self.save_dir):
+            os.mkdir(self.save_dir)
         start_time = time.time()
 
         url = "https://sakurazaka46.com/s/s46/diary/detail/36095?ima=0000&cd=blog"
@@ -304,8 +303,8 @@ class SakuraBlogArchiver:
         url = "https://sakurazaka46.com/s/s46/artist/03?ima=0000"
         self.loop(url)
 
-        url = "https://sakurazaka46.com/s/s46/diary/blog/list?ima=0000&page=0&ct=03&cd=blog"
-        self.MODE_ = MODE.BLOG_LIST
+        url = "https://sakurazaka46.com/s/s46/diary/blog/list?ima=0000&page=5&ct=03&cd=blog"
+        self.mode = MODE.BLOG_LIST
         self.loop(url)
 
         elapsed_time = time.time() - start_time
